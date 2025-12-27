b469c9a07f2ca2aa921cc1375ad44671a94e129e
rajkumara13 <rajkumararikrishnan13@gmail.com>
2025-12-27 13:09:40 +0530

    add new leetcodesum-343

--- Main.java
+++ Main.java
@@ -1,17 +1,32 @@
 package org.example;
 import java.util.*;
 class Leetcodesums2{
-    public int excelsheetnumfind(String s){
+    public int excelsheetnumfind(String s){  // leetcodesum-number(171)
          int value=0;
          for(int i=0;i<s.length();i++){
              value=value*26+(s.charAt(i)-64);
          }
          return value;
     }
+    public int integerbreak(int n){  //sum-number(343)
+        if(n==2){
+            return 1;
+        }
+        if(n==3){
+            return 4;
+        }
+        int a=1;
+        while(n>4){
+            a=a*3;
+            n=n-3;
+        }
+        a=a*n;
+        return a;
+    }
 }
     class Main{
     public static void main(String[] args) {
         Leetcodesums2 b1=new Leetcodesums2();
-        System.out.println(b1.excelsheetnumfind("ABC"));
+        System.out.println(b1.integerbreak(10));
     }
 }
