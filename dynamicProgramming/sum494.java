af7542824d49e7374413fa3d3fc5474b0824a1e1
RAJKUMAR A <rajkumararikrishnan13@gmail.com>
2026-08-16 15:59:57 +0530

    Implement solution for target sum problem

---
+++ dynamicProgramming/sum494.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int findTargetSumWays(int[] nums, int target) {
+        HashMap<Integer,Integer>dp=new HashMap<>();
+        dp.put(0,1);
+        for(int num:nums){
+            HashMap<Integer,Integer>nextDp=new HashMap<>();
+            for(int sum:dp.keySet()){
+                int count=dp.get(sum);
+                nextDp.put(sum+num,nextDp.getOrDefault(sum+num,0)+count);
+                nextDp.put(sum-num,nextDp.getOrDefault(sum-num,0)+count);
+            }
+            dp=nextDp;
+        }
+        return dp.getOrDefault(target,0);
+    }
+}
